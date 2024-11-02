f48893f4885d7f1498354f1fa541329735dcd99c
Enosxu <39416280+EnosXu@users.noreply.github.com>
2024-11-02 17:30:45 +0800

    Create Auto-Select-Network.js
    自用网络分流

---
+++ Auto-Select-Network.js
@@ -0,0 +1,22 @@
+// Auto-Select-Network.js
+
+const network = $network;
+
+// 列出需要使用 VPN 的 Wi-Fi 名称
+const vpnWiFiList = ["116", "116-5G", "leader bar", "leader bar 2", "Leader bar", "Enos 的 S24"];
+
+if (network.wifi && network.ssid) {
+  const ssid = network.ssid;
+  if (vpnWiFiList.includes(ssid)) {
+    // 如果连接的是指定的 Wi-Fi，使用代理
+    $surge.setSelectGroupPolicy("Auto", "Proxy");
+  } else {
+    // 其他情况下直连
+    $surge.setSelectGroupPolicy("Auto", "Direct");
+  }
+} else {
+  // 非 Wi-Fi 连接时默认直连
+  $surge.setSelectGroupPolicy("Auto", "Direct");
+}
+
+$done();
